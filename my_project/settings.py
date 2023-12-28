21805bc427c445d391310e1a648ae85304a0cd5c
vinuc49 <vinuchandregowda@gmail.com>
2023-12-28 16:15:00 +0000

    readies code for deploy

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k9luleit(gk8z7j3)7+*k5se4!s)340(3ylv2^odj71t!jbwz('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
