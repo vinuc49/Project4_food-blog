73e6328cf3d0b838cd05ccad4b31d5ce3d5b0892
vinuc49 <vinuchandregowda@gmail.com>
2023-12-28 23:19:57 +0000

    fix

--- my_project/urls.py
+++ my_project/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path("", include("blog.urls"), name="blog-urls"),
     path('summernote/', include('django_summernote.urls')),
     path('admin/', admin.site.urls),
+    path('', include("blog.urls"), name="blog-urls"),
 ]
