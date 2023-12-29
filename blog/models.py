6cec2b680f0644bdbf279c65337d8b562d9bb5e0
vinuc49 <vinuchandregowda@gmail.com>
2023-12-29 01:21:33 +0000

    added comment CRUD

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+def profile_page(request):
+    user = get_object_or_404(User, user=request.user)
+    comments = user.commenter.all()
 
 # Create your models here.
 STATUS = ((0, "Draft"), (1, "Published"))
