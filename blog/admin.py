e7d3bedbd73276eb3ae5697a58e021cc3509d5fa
vinuc49 <vinuchandregowda@gmail.com>
2023-12-29 19:44:04 +0000

    rest model

--- blog/admin.py
+++ blog/admin.py
@@ -1,17 +1,22 @@
 from django.contrib import admin
-from .models import Post, Comment
+from .models import Post, Comment, Restaurant
 from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
-
-    list_display = ('title', 'slug', 'status', 'created_on')
+    list_display = ('title', 'status', 'created_on')
     search_fields = ['title', 'content']
     list_filter = ('status', 'created_on')
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
 
+@admin.register(Restaurant)
+class RestaurantAdmin(SummernoteModelAdmin):
+    list_display = ('name', 'city', 'phone', 'created_on')
+    search_fields = ['name', 'city']
+    list_filter = ('city', 'county')
+    prepopulated_fields = {'restaurant_id': ('name',)}
 
 # Register your models here.
 admin.site.register(Comment)
--- blog/models.py
+++ blog/models.py
@@ -2,17 +2,31 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
-def profile_page(request):
-    user = get_object_or_404(User, user=request.user)
-    comments = user.commenter.all()
-
 # Create your models here.
 STATUS = ((0, "Draft"), (1, "Published"))
+
+class Restaurant(models.Model):
+    restaurant_id = models.SlugField(max_length=200, unique=True)
+    name = models.CharField(max_length=200, unique=True)
+    address1 = models.CharField(max_length=200)
+    address2 = models.CharField(max_length=200, null=True, blank=True)
+    city = models.CharField(max_length=50)
+    county = models.CharField(max_length=50)
+    phone = models.CharField(max_length=200)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-name"]
+
+    def __str__(self):
+        return f"{self.name} | in {self.city}"
 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, related_name="restaurant", default=None)
     featured_image = CloudinaryField('image', default='placeholder')
     content = models.TextField()
     excerpt = models.TextField(blank=True)
