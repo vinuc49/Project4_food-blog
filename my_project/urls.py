e03865c765e1c094b1f1c1b6b74f54f646d7ae89
vinuc49 <vinuchandregowda@gmail.com>
2023-12-29 00:11:18 +0000

    added authentication

--- my_project/urls.py
+++ my_project/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('summernote/', include('django_summernote.urls')),
     path('admin/', admin.site.urls),
     path('', include("blog.urls"), name="blog-urls"),
+    path("accounts/", include("allauth.urls")),
 ]
