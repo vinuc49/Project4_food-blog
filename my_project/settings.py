52120230929976ce17a333d85d2b8717e5adfcb4
vinuc49 <vinuchandregowda@gmail.com>
2023-12-30 19:41:08 +0000

    made changes in comments

--- my_project/settings.py
+++ my_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from django.contrib.messages import constants as messages
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
@@ -31,7 +32,7 @@ SECRET_KEY = 'django-insecure-k9luleit(gk8z7j3)7+*k5se4!s)340(3ylv2^odj71t!jbwz(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','localhost', '.herokuapp.com']
 
 
 # Application definition
@@ -144,6 +145,10 @@ USE_I18N = True
 
 USE_TZ = True
 
+MESSAGE_TAGS = {
+    messages.SUCCESS: 'alert-success',
+    messages.ERROR: 'alert-danger',
+}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
