b09dce3642cbcd299b27aedd3d4e67a513b4bd70
vinuc49 <vinuchandregowda@gmail.com>
2024-01-08 12:42:43 +0000

    Write test cases

---
+++ blog/test_forms.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+from .forms import CommentForm
+
+
+class TestCommentForm(TestCase):
+
+    def test_form_is_valid(self):
+        comment_form = CommentForm({'body': 'This is a great post'})
+        self.assertTrue(comment_form.is_valid())
