7a77eaef81d5d4ce3be2a60ceae90e00420b381d
vinuc49 <vinuchandregowda@gmail.com>
2024-01-10 19:43:53 +0000

    added middleware for heroku

--- my_project/settings.py
+++ my_project/settings.py
@@ -75,6 +75,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
 ]
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ROOT_URLCONF = 'my_project.urls'
 
